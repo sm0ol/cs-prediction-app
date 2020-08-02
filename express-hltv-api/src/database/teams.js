156ee49e1253038feed222a64d7de6681556dfc8
Samuel Robertson <sprobertson94@gmail.com>
2020-08-01 18:21:36 -0600

    Initial Commit

---
+++ express-hltv-api/src/database/teams.js
@@ -0,0 +1,52 @@
+const { getDatabase } = require('./mongo')
+const { ObjectID } = require('mongodb')
+const { HLTV } = require('hltv')
+
+const collectionName = 'teams';
+
+async function insertTeam(team) {
+    const database = await getDatabase()
+    const { insertedId } = await database.collection(collectionName).insertOne(team);
+    return insertedId;
+}
+
+async function getTeams() {
+    const database = await getDatabase()
+    return await database.collection(collectionName).find({}).toArray()
+}
+
+async function deleteTeam(id) {
+    const database = await getDatabase()
+    await database.collection(collectionName).deleteOne({
+        _id: new ObjectID(id)
+    })
+}
+
+async function updateTeam(id, team) {
+    const database = await getDatabase()
+    delete team._id;
+    await database.collection(collectionName).update(
+        {
+            _id: new ObjectID(id),
+        },
+        {
+            $set: {
+                ...team
+            }
+        }
+    )
+}
+
+async function getTeamById(id) {
+    const database = await getDatabase();
+    const {name, recentResults} = await HLTV.getTeam({id: id});
+    return {name, recentResults};
+}
+
+module.exports = {
+    insertTeam,
+    getTeams,
+    deleteTeam, 
+    updateTeam,
+    getTeamById
+}
---
+++ express-hltv-api/src/index.js
@@ -0,0 +1,55 @@
+//DEPENDENCIES
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+// Custom requires
+const { startDatabase } = require('./database/mongo')
+const { insertTeam, getTeams, updateTeam, deleteTeam, getTeamById } = require('./database/teams')
+
+// Define the express app
+const app = express();
+
+app.use(helmet())
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.use(morgan('combined'))
+
+app.get('/', async (req, res) => {
+    res.send(await getTeams());
+})
+
+app.post('/', async (req, res) => {
+    const newTeam = req.body;
+    await insertTeam(newTeam);
+    res.send({ message: 'New team inserted.' });
+})
+
+app.delete('/:id', async (req, res) => {
+    await deleteTeam(req.params.id);
+    res.send({ message: 'Team removed.' });
+})
+
+app.put('/:id', async (req, res) => {
+    const updatedTeam = req.body;
+    await updateTeam(req.params.id, updatedTeam);
+    res.send({ message: 'Team updated.' })
+})
+
+app.get('/team/:id', async (req, res) => {
+    let team = await getTeamById(5005)
+    res.send(team);
+})
+
+startDatabase().then(async () => {
+    await insertTeam({name: 'Complexity'});
+    app.listen(3001, () => {
+        console.log("Listening on port 3001");
+    })
+})
